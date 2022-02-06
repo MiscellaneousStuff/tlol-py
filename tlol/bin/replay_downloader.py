983d6988ab055635840262cf9b19427d200cba92
MiscellaneousStuff <joe.makepeace01@gmail.com>
2022-02-06 01:30:16 +0000

    Added additional flags to replay file downloader

--- tlol/bin/replay_downloader.py
+++ tlol/bin/replay_downloader.py
@@ -38,6 +38,10 @@ flags.DEFINE_string("target_patch", "11_23", "U.GG formatted target League patch
 flags.DEFINE_integer("max_games", -1, \
     """(Default: Max returned games)
     Limits the number of replay files downloaded up to a maximum number of games.""")
+flags.DEFINE_integer("max_workers", 10, "Max workers per process")
+flags.DEFINE_float("delay", 0.5, "Request delay")
+flags.DEFINE_string("outfile", "", "(Optional) Save match ID list to a text file")
+flags.DEFINE_string("infile", "", "(Optional) Use a text file of match IDs")
 
 flags.DEFINE_integer("start_page", 1, \
     "Sets the first leaderboard page which is scraped")
@@ -50,34 +54,47 @@ def main(unused_argv):
     # Setup APIs
     u_gg       = U_GG_API()
     downloader = ReplayDownloader()
-
-    # Get top players on EUW Ranked/Solo Duo leaderboard
+    
+     # Get top players on EUW Ranked/Solo Duo leaderboard
     summoners = u_gg.get_leaderboard(
         page_start=FLAGS.start_page,
         page_end=FLAGS.last_page,
         region="euw1",
-        max_workers=10)
+        max_workers=FLAGS.max_workers,
+        delay=FLAGS.delay)
     
     # Extract summoner names
-    summoner_names = [s["summonerName"]
-                      for s in summoners]
+    print('Summoner count:', len(summoners))
+    summoner_names = [s["summonerName"] if "summonerName" in s else ""
+                    for s in summoners]
+
     print(summoner_names)
 
-    # Get matches for above summoners matching specific criteria
-    matches = u_gg.get_matches(
-        summoner_names=summoner_names,
-        champs=FLAGS.champs.split(","), # ["Caitlyn"],
-        target_patch=FLAGS.target_patch,
-        outpath="", # set the outfile to write the match ids to a file
-        win_only=False,
-        max_workers=10,
-        seasonIds=[17, 16])
-    matches = list(matches)
+    if not FLAGS.infile:
+        # Get matches for above summoners matching specific criteria
+        matches = u_gg.get_matches(
+            summoner_names=summoner_names,
+            champs=FLAGS.champs.split(","), # ["Caitlyn"],
+            target_patch=FLAGS.target_patch,
+            outpath="", # set the outfile to write the match ids to a file
+            win_only=False,
+            max_workers=FLAGS.max_workers,
+            seasonIds=[18],
+            delay=FLAGS.delay)
+        matches = list(matches)
+    else:
+        with open(FLAGS.infile) as f:
+            matches = f.read().split("\n")
+
+    if FLAGS.outfile:
+        with open(FLAGS.outfile, "w+") as f:
+            f.write("\n".join([str(m) for m in matches]))
+
     if FLAGS.max_games != -1:
         matches = matches[0:FLAGS.max_games]
-    
+
     print(matches)
-    
+
     # Download all games
     for game_id in matches:
         req = downloader.download(game_id)
