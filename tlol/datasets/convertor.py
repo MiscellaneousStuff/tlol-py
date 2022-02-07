4203105e8675e679360a68a904d83b885124fd23
MiscellaneousStuff <joe.makepeace01@gmail.com>
2022-02-07 23:42:13 +0000

    Updated convertor for new replay DB scheme

--- tlol/datasets/convertor.py
+++ tlol/datasets/convertor.py
@@ -41,32 +41,20 @@ CREATE_CHAMP_TABLE = """CREATE TABLE champs (
                         name TEXT,
                         health REAL,
                         max_health REAL,
-                        max_mana REAL,
                         team INTEGER,
-                        crit REAL,
-                        critMulti REAL,
-                        ap REAL,
                         armour REAL,
                         mr REAL,
                         movement_speed REAL,
                         is_alive INTEGER,
-                        duration REAL,
                         position_x REAL,
                         position_y REAL,
                         position_z REAL,
-                        prev_position_x REAL,
-                        prev_position_y REAL,
-                        prev_position_z REAL,
-                        ability_haste REAL,
-                        lethality REAL,
-                        direction REAL,
-                        experience REAL,
-                        mana_regen REAL,
-                        health_regen REAL,
-                        transformation TEXT,
+
+                        is_moving INTEGER,
                         targetable INTEGER,
                         invulnerable INTEGER,
                         recallState INTEGER,
+
                         q_name TEXT,
                         q_level INTEGER,
                         q_cd FLOAT,
@@ -86,7 +74,23 @@ CREATE_CHAMP_TABLE = """CREATE TABLE champs (
                         f_name TEXT,
                         f_level INTEGER,
                         f_cd FLOAT,
-                        f_summoner_spell_type INTEGER
+                        f_summoner_spell_type INTEGER,
+
+                        crit REAL,
+                        critMulti REAL,
+                        level INTEGER,
+                        mana REAL,
+                        max_mana REAL,
+                        ability_haste REAL,
+                        ap REAL,
+                        lethality REAL,
+                        experience REAL,
+                        mana_regen REAL,
+                        health_regen REAL,
+                        attack_range REAL,
+
+                        current_gold REAL,
+                        total_gold REAL
                         )"""
 
 CREATE_OBJ_TABLE   = """CREATE TABLE objects (
@@ -98,32 +102,47 @@ CREATE_OBJ_TABLE   = """CREATE TABLE objects (
                         name TEXT,
                         health REAL,
                         max_health REAL,
-                        max_mana REAL,
                         team INTEGER,
-                        crit REAL,
-                        critMulti REAL,
-                        ap REAL,
                         armour REAL,
                         mr REAL,
                         movement_speed REAL,
                         is_alive INTEGER,
-                        duration REAL,
                         position_x REAL,
                         position_y REAL,
                         position_z REAL,
-                        prev_position_x REAL,
-                        prev_position_y REAL,
-                        prev_position_z REAL,
-                        ability_haste REAL,
-                        lethality REAL,
-                        direction REAL,
-                        experience REAL,
-                        mana_regen REAL,
-                        health_regen REAL,
-                        transformation TEXT,
+
+                        is_moving INTEGER,
                         targetable INTEGER,
                         invulnerable INTEGER,
                         recallState INTEGER
+                        )"""
+
+CREATE_MISSILE_TABLE = """CREATE TABLE missiles (
+                        game_id INTEGER,
+                        time REAL,
+                        obj_type TEXT,
+                        net_id INTEGER,
+                        obj_id INTEGER,
+                        name TEXT,
+                        health REAL,
+                        max_health REAL,
+                        team INTEGER,
+                        armour REAL,
+                        mr REAL,
+                        movement_speed REAL,
+                        is_alive INTEGER,
+                        position_x REAL,
+                        position_y REAL,
+                        position_z REAL,
+
+                        start_position_x REAL,
+                        start_position_y REAL,
+                        start_position_z REAL,
+                        end_position_x REAL,
+                        end_position_y REAL,
+                        end_position_z REAL,
+                        source_idx INTEGER,
+                        destination_idx INTEGER
                         )"""
 
 def handle_nan(x):
@@ -142,7 +161,7 @@ def insert_objs(game_id, obs, cur, time, table):
         try:
             if table == "champs":
                 cur.execute(f"""INSERT INTO champs VALUES (
-                    {game_id},
+                   {game_id},
                     {time},
                     '{table}',
                     {c["net_id"]},
@@ -150,32 +169,20 @@ def insert_objs(game_id, obs, cur, time, table):
                     '{handle_str(c["name"])}',
                     {handle_nan(c["health"])},
                     {handle_nan(c["max_health"])},
-                    {handle_nan(c["max_mana"])},
                     {int(c["team"])},
-                    {handle_nan(c["crit"])},
-                    {handle_nan(c["critMulti"])},
-                    {handle_nan(c["ap"])},
                     {handle_nan(c["armour"])},
                     {handle_nan(c["mr"])},
                     {handle_nan(c["movement_speed"])},
                     {1 if c["is_alive"] else 0},
-                    {handle_nan(c["duration"])},
                     {handle_nan(c["position"]["x"])},
                     {handle_nan(c["position"]["y"])},
                     {handle_nan(c["position"]["z"])},
-                    {handle_nan(c["prev_position"]["x"])},
-                    {handle_nan(c["prev_position"]["y"])},
-                    {handle_nan(c["prev_position"]["z"])},
-                    {handle_nan(c["ability_haste"])},
-                    {handle_nan(c["lethality"])},
-                    {handle_nan(c["direction"])},
-                    {handle_nan(c["experience"])},
-                    {handle_nan(c["mana_regen"])},
-                    {handle_nan(c["health_regen"])},
-                    '{c["transformation"]}',
+
+                    {1 if c["is_moving"] else 0},
                     {1 if c["targetable"] else 0},
                     {1 if c["invulnerable"] else 0},
                     {c["recallState"]},
+
                     '{c["Q"]["name"]}',
                     {c["Q"]["level"]},
                     {c["Q"]["cd"]},
@@ -195,8 +202,54 @@ def insert_objs(game_id, obs, cur, time, table):
                     '{c["F"]["name"]}',
                     {c["F"]["level"]},
                     {c["F"]["cd"]},
-                    {c["F"]["summoner_spell_type"]}
+                    {c["F"]["summoner_spell_type"]},
+                    
+                    {handle_nan(c["crit"])},
+                    {handle_nan(c["crit_multi"])},
+                    {handle_nan(c["level"])},
+                    {handle_nan(c["mana"])},
+                    {handle_nan(c["max_mana"])},
+                    {handle_nan(c["ability_haste"])},
+                    {handle_nan(c["ap"])},
+                    {handle_nan(c["lethality"])},
+                    {handle_nan(c["experience"])},
+                    {handle_nan(c["mana_regen"])},
+                    {handle_nan(c["health_regen"])},
+                    {handle_nan(c["attack_range"])},
+
+                    {handle_nan(c["current_gold"])},
+                    {handle_nan(c["total_gold"])}
                     )""")
+            
+            elif table == "missiles":
+                cur.execute(f"""INSERT INTO missiles VALUES (
+                    {game_id},
+                    {time},
+                    '{table}',
+                    {c["net_id"]},
+                    {c["obj_id"]},
+                    '{handle_str(c["name"])}',
+                    {handle_nan(c["health"])},
+                    {handle_nan(c["max_health"])},
+                    {int(c["team"])},
+                    {handle_nan(c["armour"])},
+                    {handle_nan(c["mr"])},
+                    {handle_nan(c["movement_speed"])},
+                    {1 if c["is_alive"] else 0},
+                    {handle_nan(c["position"]["x"])},
+                    {handle_nan(c["position"]["y"])},
+                    {handle_nan(c["position"]["z"])},
+
+                    {handle_nan(c["start_pos"]["x"])},
+                    {handle_nan(c["start_pos"]["y"])},
+                    {handle_nan(c["start_pos"]["z"])},
+                    {handle_nan(c["end_pos"]["x"])},
+                    {handle_nan(c["end_pos"]["y"])},
+                    {handle_nan(c["end_pos"]["z"])},
+                    {int(c["src_id"])},
+                    {int(c["dest_id"])}
+                    )""")
+            
             else:
                 cur.execute(f"""INSERT INTO objects VALUES (
                     {game_id},
@@ -207,33 +260,20 @@ def insert_objs(game_id, obs, cur, time, table):
                     '{handle_str(c["name"])}',
                     {handle_nan(c["health"])},
                     {handle_nan(c["max_health"])},
-                    {handle_nan(c["max_mana"])},
                     {int(c["team"])},
-                    {handle_nan(c["crit"])},
-                    {handle_nan(c["critMulti"])},
-                    {handle_nan(c["ap"])},
                     {handle_nan(c["armour"])},
                     {handle_nan(c["mr"])},
                     {handle_nan(c["movement_speed"])},
                     {1 if c["is_alive"] else 0},
-                    {handle_nan(c["duration"])},
                     {handle_nan(c["position"]["x"])},
                     {handle_nan(c["position"]["y"])},
                     {handle_nan(c["position"]["z"])},
-                    {handle_nan(c["prev_position"]["x"])},
-                    {handle_nan(c["prev_position"]["y"])},
-                    {handle_nan(c["prev_position"]["z"])},
-                    {handle_nan(c["ability_haste"])},
-                    {handle_nan(c["lethality"])},
-                    {handle_nan(c["direction"])},
-                    {handle_nan(c["experience"])},
-                    {handle_nan(c["mana_regen"])},
-                    {handle_nan(c["health_regen"])},
-                    '{c["transformation"]}',
+                    {1 if c["is_moving"] else 0},
                     {1 if c["targetable"] else 0},
                     {1 if c["invulnerable"] else 0},
                     {c["recallState"]}
                     )""")
+
         except Exception as e:
             print(e, handle_str(c["name"]))
             print('check for \\u:', "\\u" in c["name"])
@@ -258,20 +298,24 @@ def insert_game(cur_fi, cur):
             insert_objs(game_id, obs, cur, time, "others")
 
 def convert_dataset(json_dir, db_dir):
-    json_files = os.listdir(json_dir)
+    json_files = os.listdir(json_dir)[0:1]
 
     db_path = os.path.join(db_dir, 'replay.db')
     con = sqlite3.connect(db_path)
     cur = con.cursor()
     cur.execute(CREATE_GAME_TABLE)
     cur.execute(CREATE_CHAMP_TABLE)
+    cur.execute(CREATE_MISSILE_TABLE)
     cur.execute(CREATE_OBJ_TABLE)
 
     cur.execute("BEGIN;")
 
     for json_fi in json_files:
         cur_fi = os.path.join(json_dir, json_fi)
-        insert_game(cur_fi, cur)
+        try:
+            insert_game(cur_fi, cur)
+        except Exception as e:
+            print(e)
     
     cur.execute("COMMIT;")
 
