cefcef32478da7bd13b20bb1ad5b6e09497ed87b
Joe Makepeace <joe.makepeace01@gmail.com>
2022-05-12 00:30:46 +0100

    setup movement prediction from jinx position skeleton

--- tlol/bin/train.py
+++ tlol/bin/train.py
@@ -27,6 +27,8 @@ import random
 import numpy as np
 
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
 from absl import app
 from absl import flags
@@ -37,54 +39,90 @@ FLAGS = flags.FLAGS
 flags.DEFINE_string("db_dir", None, "Directory of replay DBs to convert")
 flags.mark_flag_as_required("db_dir")
 
-def test(obs_s, act_s):
-    pass
 
-def train(obs_s, act_s):
-    pass
+class JinxPolicy(nn.Module):
 
-def main(unused_argv):
-    db_dir = FLAGS.db_dir
-    
-    seed = 1
-    random.seed(seed)
-    torch.manual_seed(seed)
-    np.seed(seed)
+    def __init__(self, in_dim, out_dim):
+        super(JinxPolicy, self).__init__()
+        self.out_dim = out_dim
+        self.fc1 = nn.Linear(in_dim, in_dim // 2)
+        self.fc2 = nn.Linear(in_dim // 2, in_dim // 4)
+        self.fc3 = nn.Linear(in_dim // 4, out_dim)
 
-    dataset = TLoLReplayDataset(db_dir)
-    device = 'cuda' if torch.cuda.is_available() else "cpu"
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        x = F.log_softmax(x, dim=1)
+        return x
 
-    dataloader = torch.utils.data.DataLoader(
-                    dataset,
-                    shuffle=True,
-                    pin_memory=(device=="cuda"),
-                    num_workers=6,
-                    batch_size=32,
-                    collate_fn=dataset.collate_fixed_length)
-    
-    train_end = int(len(dataset) * 0.9)
 
-    train_set = torch.utils.data.Subset(
-        dataloader,
-        list(range(0, train_end)))
-    
-    test_set = torch.utils.data.Subset(
-        dataloader,
-        list(range(train_end, len(dataset))))
+def test(model, epoch, test_loader, device, batch_count):
+    model.eval()
+    return 0
 
-    # batch = next(iter(dataloader))
-    batch_count = math.ceil(len(dataset) / 32)
-    for i, batch in enumerate(train_set):
-        print(f"{i+1}/{batch_count}")
+def train(model, epoch, train_loader, device, batch_count):
+    model.train()
+    batch_idx = 0
+
+    for batch in train_loader:
+        print(f"Epoch: {epoch+1}, Batch: {batch_idx+1}/{batch_count}")
 
         batch_obs_s = batch["obs_s"]
         batch_act_s = batch["act_s"]
 
-        obs_s = batch_obs_s[:, :, :].to(device)
-        act_s = batch_act_s[:, :, :].to(device)
+        obs_s = batch_obs_s[:, :, 9:12].to(device)
+        act_s = batch_act_s[:, :, -2:].to(device)
 
-        train(obs_s, act_s)
-        test(obs_s, act_s)
+        batch_idx += 1
+
+def main(unused_argv):
+    db_dir = FLAGS.db_dir
+    batch_size = 32
+    n_epochs = 1
+    
+    seed = 1
+    random.seed(seed)
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+
+    dataset = TLoLReplayDataset(db_dir)
+    device = 'cuda' if torch.cuda.is_available() else "cpu"
+
+    train_end = int(len(dataset) * 0.9)
+
+    train_set = torch.utils.data.Subset(
+        dataset,
+        list(range(0, train_end)))
+
+    test_set = torch.utils.data.Subset(
+        dataset,
+        list(range(train_end, len(dataset))))
+
+    train_loader = torch.utils.data.DataLoader(
+                    train_set,
+                    shuffle=True,
+                    pin_memory=(device=="cuda"),
+                    num_workers=6,
+                    batch_size=batch_size,
+                    collate_fn=dataset.collate_fixed_length)
+
+    test_loader = torch.utils.data.DataLoader(
+                    test_set,
+                    shuffle=False,
+                    pin_memory=(device=="cuda"),
+                    num_workers=6,
+                    batch_size=batch_size,
+                    collate_fn=dataset.collate_fixed_length)
+
+    # batch = next(iter(train_loader))
+    batch_count = math.ceil(len(train_set) / batch_size)
+    
+    model = JinxPolicy(in_dim=2, out_dim=2)
+
+    for epoch in range(n_epochs):
+        train(model, epoch, train_loader, device, batch_count)
+        loss = test(model, epoch, test_loader, device, batch_count)
 
 def entry_point():
     app.run(main)
