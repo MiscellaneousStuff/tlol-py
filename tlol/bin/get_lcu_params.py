9f7b2d0410d9e1eb237f6401824f18443f193492
MiscellaneousStuff <joe.makepeace01@gmail.com>
2023-02-09 05:02:11 +0000

    Added script to print remoting auth token and app port for running and logged in client

---
+++ tlol/bin/get_lcu_params.py
@@ -0,0 +1,30 @@
+# MIT License
+# 
+# Copyright (c) 2023 MiscellaneousStuff
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+"""Prints the app-port and remoting-auth-token for a running League of Legends
+process."""
+
+from tlol.replays.downloader import ReplayDownloader
+
+if __name__ == "__main__":
+    replay_downloader = ReplayDownloader()
+    print("Remoting Auth Token:", replay_downloader.remoting_auth_token)
+    print("App Port:", replay_downloader.app_port)
